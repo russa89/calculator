23fc22beb9ece73dd42a86bbe7853ec7261f28b5
russa89 <natast2189@gmail.com>
2024-06-20 15:39:00 +0300

    done

---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,32 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class CalculatorService {
+    public String answerHello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int additionResult = num1 + num2;
+        return num1 + " + " + num2 + " = " + additionResult;
+    }
+
+    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int subtractionResult = num1 - num2;
+        return num1 + " - " + num2 + " = " + subtractionResult;
+    }
+
+    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int multiplyResult = num1 * num2;
+        return num1 + " * " + num2 + " = " + multiplyResult;
+    }
+
+    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int divideResult = num1 / num2;
+        return num1 + " / " + num2 + " = " + divideResult;
+    }
+
+}
